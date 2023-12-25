64389bd976c422592528d5cece317f962f349763
Wahab Tlais <wahabtlais@gmail.com>
2023-12-25 23:32:10 +0200

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.CONNECTION)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 const app = express();
 
