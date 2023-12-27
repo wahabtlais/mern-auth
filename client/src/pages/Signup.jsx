b17c7b57f0ef375b40a364c45eaa070be6f3b2e4
Wahab Tlais <wahabtlais@gmail.com>
2023-12-27 18:41:22 +0200

    Complete signup page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,30 +1,66 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setLoading(true);
+			setError(false);
+			const res = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			setLoading(false);
+			if (data.success === false) {
+				setError(true);
+				return;
+			}
+		} catch (error) {
+			setLoading(false);
+			setError(true);
+		}
+	};
 	return (
 		<div className="p-3 max-w-lg mx-auto">
 			<h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
-			<form className="flex flex-col gap-4">
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
 				<input
 					type="text"
 					placeholder="Username"
 					id="username"
 					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
 				/>
 				<input
 					type="email"
 					placeholder="Email"
 					id="email"
 					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
 				/>
 				<input
 					type="password"
 					placeholder="Password"
 					id="password"
 					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
 				/>
-				<button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-					SignUp
+				<button
+					disabled={loading}
+					className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+				>
+					{loading ? "Loading..." : "SignUp"}
 				</button>
 			</form>
 			<div className="flex text-sm p-1 mt-2">
@@ -33,6 +69,9 @@ const Signup = () => {
 					<span className="text-blue-500 ml-1">SignIn</span>
 				</Link>
 			</div>
+			<p className="text-red-600 text-center py-2 font-semibold">
+				{error && "Failed to sign up."}
+			</p>
 		</div>
 	);
 };
