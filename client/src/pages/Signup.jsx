b35e1aa9bd09ee394983df644d6ec4d68eea098b
Wahab Tlais <wahabtlais@gmail.com>
2023-12-29 16:47:38 +0200

    Complete Sign In page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
 	const [formData, setFormData] = useState({});
 	const [error, setError] = useState(false);
 	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 	const handleChange = (e) => {
 		setFormData({ ...formData, [e.target.id]: e.target.value });
 	};
@@ -26,6 +27,7 @@ const Signup = () => {
 				setError(true);
 				return;
 			}
+			navigate("/sign-in");
 		} catch (error) {
 			setLoading(false);
 			setError(true);
@@ -33,7 +35,7 @@ const Signup = () => {
 	};
 	return (
 		<div className="p-3 max-w-lg mx-auto">
-			<h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+			<h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
 			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
 				<input
 					type="text"
@@ -60,7 +62,7 @@ const Signup = () => {
 					disabled={loading}
 					className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
 				>
-					{loading ? "Loading..." : "SignUp"}
+					{loading ? "Loading..." : "Sign Up"}
 				</button>
 			</form>
 			<div className="flex text-sm p-1 mt-2">
