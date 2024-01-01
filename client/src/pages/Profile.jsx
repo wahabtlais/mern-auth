09a5dc6c64aab40fa8fa46e0332c130ce4f1e362
Wahab Tlais <wahabtlais@gmail.com>
2024-01-01 11:53:45 +0200

    Complete profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,50 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-	return <div>Profile</div>;
+	const { currentUser } = useSelector((state) => state.user);
+	return (
+		<div className="p-3 max-w-lg mx-auto">
+			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+			<form className="flex flex-col gap-4">
+				<img
+					src={currentUser.profilePicture}
+					alt="profile"
+					className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
+				/>
+				<input
+					defaultValue={currentUser.username}
+					type="text"
+					id="username"
+					placeholder="Username"
+					className="bg-slate-200 rounded-lg p-3"
+				/>
+				<input
+					defaultValue={currentUser.email}
+					type="email"
+					id="email"
+					placeholder="Email"
+					className="bg-slate-200 rounded-lg p-3"
+				/>
+				<input
+					type="text"
+					id="username"
+					placeholder="Username"
+					className="bg-slate-200 rounded-lg p-3"
+				/>
+				<button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+					Update
+				</button>
+			</form>
+			<div className="flex justify-between mt-4">
+				<span className="text-red-600 cursor-pointer font-semibold">
+					Delete Account
+				</span>
+				<span className="text-red-600 cursor-pointer font-semibold">
+					Sign out
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
